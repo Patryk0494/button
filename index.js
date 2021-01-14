ed9c39341242d6aa93376f8d5c23234a5887a87b
Patryk <jpatryk0494@gmail.com>
2021-01-14 22:22:47 +0100

    changed inner button text

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ let darkMode = false;
 button.addEventListener('click', function() {
     if (darkMode) {
         button.classList.remove('button--darkMode');
+        button.innerHTML = 'Dark Mode'
         background.classList.remove('background--darkMode');
         return darkMode = false;
     } else {
         button.classList.add('button--darkMode');
+        button.innerHTML = 'Light Mode'
         background.classList.add('background--darkMode');
         return darkMode = true;
     }
